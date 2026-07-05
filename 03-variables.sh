5eca9590c23a36c3fdbd7b8fcbf104615cc4ca26
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-05 20:07:24 +0530

    arguments

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ PERSON1="RAMU"
 PERSON2="LAXMAN"
 
 echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hello $PERSON1"
+echo "${PERSON2}:: Hello $PERSON1"
 echo "$PERSON1:: How are you $PERSON2"
 echo "$PERSON2:: I am fyn $PERSON1"
