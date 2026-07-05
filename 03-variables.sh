3f9a93eeaf25071debd85d6b00af6c697f0395d5
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-05 20:04:33 +0530

    Hello

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="RAMU"
+PERSON2="LAXMAN"
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: I am fyn $PERSON1"
