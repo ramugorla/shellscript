bb31e79f4e5de519b6b5cb6c750330a907219b1a
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-06 22:53:23 +0530

    read command

---
+++ 08-special-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+echo "All variables passed:: $@"
+echo "Number of var passed:: $#"
+echo "Current script name:: $0"
+echo "Present Working Directory:: $PWD"
+echo "Home directory of current user:: $HOME"
+echo "User name of current sctipt:: $USER"
+echo "PID of current script:: $$"
+echo "PID of command running in backgroud:: $!"
