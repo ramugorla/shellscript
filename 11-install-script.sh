f6e3bfc177f74e9068a56487c4685d6b5459fdff
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-06 23:18:09 +0530

    read command

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -2,9 +2,15 @@
 
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[37m"
+
+
 if [ $USERID -ne 0 ]
 then
-    echo "Error:: You must have root access to execute script"
+    echo -e " $R Error::$N You must have root access to execute script"
     exit 1
 fi
 
@@ -12,15 +18,15 @@ fi
 dnf install mysql -y 
 if [ $? -ne 0 ]
 then
-    echo "Installing Mysql ... Failure"
+    echo -e "Installing Mysql ... $R Failure $N"
 else
-    echo "Installing Mysql ... Success"
+    echo -e "Installing Mysql ... $G Success $N"
 fi 
 
 dnf install git -y 
 if [ $? -ne 0 ]
 then
-    echo "Installing Mysql ... Failure"
+    echo -e "Installing Git ... $R Failure $N"
 else
-    echo "Installing Mysql ... Success"
+    echo -e "Installing Git ... $G Success $N"
 fi
