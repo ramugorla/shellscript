4fa0c021e499c13adf4b8123fff186582612ed43
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-06 23:03:56 +0530

    read command

---
+++ 10-conditions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+# Print the given number is even or odd 
+
+NUMBER=$1
+G="\e[32m"
+N="\e[0m"
+R="\e[31m"
+
+
+
+if [ $NUMBER % 2 -eq 0 ]
+then
+    echo -e "Given number $NUMBER is $G EVEN $N"
+else
+    echo -e "Given number $NUMBER is $R ODD $N"
+fi 
+
