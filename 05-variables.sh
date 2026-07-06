691e6e4f36873a783a10d159749a937a02246280
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-06 22:23:25 +0530

    read command

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,7 +4,11 @@ echo "Please Enter your user name:: "
 
 read -s USERNAME
 
+echo "Username:: $USERNAME"
+
 echo "Please enter your password:: "
 
 read -s PASSWORD 
+
+echo "Password:: $PASSWORD"
 
