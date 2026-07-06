90494eb8c3acdfa50ddc17c72d5a5465c5da8127
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-06 22:48:25 +0530

    read command

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+MOVIES=("RRR", "Devara", "Kushi")
+
+echo "First Movie is :: $MOVIES[0]"
+echo "Second Movie is :: $MOVIES[1]"
+echo "Third Movie is :: $MOVIES[2]"
+echo "All Moviee are :: $MOVIES[@]"
+
