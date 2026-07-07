694770e9cccac67e398b1cf821b2badde3cfe360
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-07 23:06:41 +0530

    log files

--- 15-logs.sh
+++ 15-logs.sh
@@ -14,7 +14,7 @@ LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOGS_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-mkdir -p $LOGS_FOLDER
+mkdir -p var/log/expense-logs
 echo $LOG_FILE
 echo $TIMESTAMP
 
