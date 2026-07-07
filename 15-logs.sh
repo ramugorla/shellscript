24bd71191e91355f4a2f32f2095b9a0a986eb2b3
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-07 23:01:38 +0530

    log files

---
+++ 15-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $? -ne 0 ]
+then
+    echo "You must have root access to execute this script"
+    exit 1
+fi 
+
+
+LOGS_FOLDER="var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGS_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+echo $LOG_FILE
+echo $TIMESTAMP
