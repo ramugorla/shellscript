2785836c7db84d91fc92543ccab4ae8aa7e6cf4a
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-08 22:44:51 +0530

    loops

--- 20-backup.sh
+++ 20-backup.sh
@@ -15,7 +15,7 @@ LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-if [ $# -ne 2 ]
+if [ $# -lt 2 ]
 then
     USAGE
 fi 
