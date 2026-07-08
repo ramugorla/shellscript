34203b57b1cc09e4e47d61688e5e31581f61b3d9
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-08 22:36:37 +0530

    loops

--- 20-backup.sh
+++ 20-backup.sh
@@ -7,6 +7,7 @@ DAYS=${3:-14}
 
 USAGE(){
     echo "sh 20-backup.sh <SOURCE_DIT> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
 }
 
 LOGS_FOLDER="/var/log/shellscript-logs"
@@ -17,4 +18,14 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 if [ $# -ne 2 ]
 then
     USAGE
+fi 
+
+if [ -d $SOURCE_DIR -ne 0 ]
+then 
+    echo "$SOURCE_DIR doesn't exist ... please check"
+fi 
+
+if [ -d $DEST_DIR -ne 0 ]
+then 
+    echo "$DEST_DIR doesn't exist ... please check"
 fi 
