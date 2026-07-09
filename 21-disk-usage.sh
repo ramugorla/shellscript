6356b9f4da437b67cbb650497f33151de9892a24
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-07-09 22:06:35 +0530

    command

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 
-USAGE=$(df -hT | grep xfs)
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESSHOLD=5
 
 while read -r line
 do 
-    echo $line
-done <<< $USAGE
+    USAGE=$(echo $line | awk -F " " '{print $6F}')
+    PARTION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partion is $PARTION, and Usage is $USAGE"
+
+done <<< $DISK_USAGE
